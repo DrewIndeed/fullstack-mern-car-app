25047fc4c4a570c3640e977975de31ef70c602a9
Andrew Le <s3820098@rmit.edu.vn>
2022-06-04 11:38:36 +0700

    12. yarn add + create Book Card
    @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome

--- car-react-frontend/src/app/containers/HomePage/index.tsx
+++ car-react-frontend/src/app/containers/HomePage/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import BookCard from "../../components/bookCard";
 import Navbar from "../../components/navbar/index";
 import TopSection from "./TopSection";
 
@@ -20,6 +21,7 @@ export default function HomePage() {
     <PageContainer>
       <Navbar />
       <TopSection />
+      <BookCard />
     </PageContainer>
   );
 }
